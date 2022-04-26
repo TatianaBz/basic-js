817a870e98eb5915e36ed03529b14e27cbb6db3d
tatiana <name>
2022-04-26 22:45:22 +0700

    init: task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,13 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,10 +15,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arrays) {
+    sum = 1;
+      for (let i = 0; i < arrays.length; i++) {
+        if (Array.isArray (arrays[i])) {
+          arrays[i] = deepCount(arrays[i]);
+          sum ++;
+        }
+      }
+      return sum;
   }
+
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(getSeason) {
+  if (typeof getSeason == undefined)
+    console.log('Unable to determine the time of year!');
+  else {
+    if (getSeason instanceof Date) {
+      let date = getSeason.getMonth();
+      switch (date) {
+        case date < 2: {
+          console.log('winter');
+          break;
+        }
+        case date < 5: {
+          console.log('spring');
+          break;
+        }
+        case date < 8: {
+          console.log(' summer');
+          break;
+        }
+        case date < 11: {
+          console.log('autumn');
+          break;
+        }
+        default:
+          console.log('winter');
+      }
+    } else
+      console.log('throw!ErrorInvalid date!');
+
+  }
 }
 
 module.exports = {
