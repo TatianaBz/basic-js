dea291df93bb84f77505e261ad23c5fb86edce66
tatiana <name>
2023-12-23 19:20:33 +0700

    task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,10 +1,8 @@
-const {
-    NotImplementedError
-} = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *
+ *npm
  * @param {String} str string to repeat
  * @param {Object} options options object
  * @return {String} repeating string
@@ -16,65 +14,64 @@ const {
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
- */function repeater(str, options) {
-    let repeatRaport = {};
-    let result = [];
-    let repeatResult = [];
-    let repeat;
-    result[0] =  String(str);
-    let optionsDef = {
-        repeatTimes: 1,
-        separator: '+',
-        addition: '',
-        additionRepeatTimes: 1,
-        additionSeparator: '|'
-    };
-    if (options == '') {
-        repeatRaport = optionsDef;
+ */ function repeater(str, options) {
+  let repeatRaport = {};
+  let result = [];
+  let repeatResult = [];
+  let repeat;
+  result[0] = String(str);
+  let optionsDef = {
+    repeatTimes: 1,
+    separator: "+",
+    addition: "",
+    additionRepeatTimes: 1,
+    additionSeparator: "|",
+  };
+  if (options == "") {
+    repeatRaport = optionsDef;
+  } else {
+    if (options.separator !== undefined) {
+      repeatRaport.separator = String(options.separator);
     } else {
-        if (options.separator !== undefined) {
-            repeatRaport.separator = String(options.separator);
-        } else {
-            repeatRaport.separator = optionsDef.separator;
-        }
-        if (options.additionRepeatTimes) {
-            repeatRaport.additionRepeatTimes = options.additionRepeatTimes;
-        } else {
-            repeatRaport.additionRepeatTimes = optionsDef.additionRepeatTimes;
-        }
-
-        if (options.additionSeparator !== undefined) {
-            repeatRaport.additionSeparator = String(options.additionSeparator);
-        } else {
-            repeatRaport.additionSeparator = optionsDef.additionSeparator;
-        }
-
-        if (options.repeatTimes) {
-            repeatRaport.repeatTimes = options.repeatTimes;
-        } else {
-            repeatRaport.repeatTimes = optionsDef.repeatTimes;
-            repeatRaport.separator = '';
-        }
-        if (options.addition !== undefined) {
-            repeatRaport.addition = String(options.addition);
-        } else {
-            repeatRaport.addition = optionsDef.addition;
-            repeatRaport.additionSeparator = '';
-        }
+      repeatRaport.separator = optionsDef.separator;
     }
-    for (let j = 0; j < repeatRaport.additionRepeatTimes - 1; j++) {
-        result.push(repeatRaport.addition, repeatRaport.additionSeparator);
+    if (options.additionRepeatTimes) {
+      repeatRaport.additionRepeatTimes = options.additionRepeatTimes;
+    } else {
+      repeatRaport.additionRepeatTimes = optionsDef.additionRepeatTimes;
+    }
 
+    if (options.additionSeparator !== undefined) {
+      repeatRaport.additionSeparator = String(options.additionSeparator);
+    } else {
+      repeatRaport.additionSeparator = optionsDef.additionSeparator;
     }
-    result.push(repeatRaport.addition);
-    repeat = result.join('');
-    for (let i = 0; i < repeatRaport.repeatTimes - 1; i++) {
-        repeatResult.push(repeat, repeatRaport.separator);
+
+    if (options.repeatTimes) {
+      repeatRaport.repeatTimes = options.repeatTimes;
+    } else {
+      repeatRaport.repeatTimes = optionsDef.repeatTimes;
+      repeatRaport.separator = "";
     }
-    repeatResult.push(repeat);
-    return repeatResult.join('');
+    if (options.addition !== undefined) {
+      repeatRaport.addition = String(options.addition);
+    } else {
+      repeatRaport.addition = optionsDef.addition;
+      repeatRaport.additionSeparator = "";
+    }
+  }
+  for (let j = 0; j < repeatRaport.additionRepeatTimes - 1; j++) {
+    result.push(repeatRaport.addition, repeatRaport.additionSeparator);
+  }
+  result.push(repeatRaport.addition);
+  repeat = result.join("");
+  for (let i = 0; i < repeatRaport.repeatTimes - 1; i++) {
+    repeatResult.push(repeat, repeatRaport.separator);
+  }
+  repeatResult.push(repeat);
+  return repeatResult.join("");
 }
 
 module.exports = {
-    repeater
+  repeater,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,75 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(cod) {
+    this.cod = "";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  er() {
+    if (
+      message == undefined ||
+      encryptedMessage == undefined ||
+      key == undefined
+    ) {
+      throw new Error("Incorrect arguments!");
+    }
+  }
+  encrypt(message, key) {
+    if (this.cod == true || this.cod == "") {
+      let arrMessage = message.toUpperCase().split("");
+      let arrkey = key.repeat(Math.ceil(message.length / key.length)).split("");
+      let answer = [];
+      let i = 0;
+      arrMessage.map((e) => {
+        if (e.codePointAt(0) >= 65 && e.codePointAt(0) <= 90) {
+          let elem = e.codePointAt(0) - 65 + (arrkey[i].codePointAt(0) - 97);
+          i++;
+          if (elem > 26) elem -= 26;
+          answer.push(String.fromCharCode(elem + 65));
+        } else answer.push(e);
+      });
+      return answer.join("");
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (this.cod == false) {
+      let arrMessage = encryptedMessage.toUpperCase().split("");
+      let arrkey = key
+        .repeat(Math.ceil(encryptedMessage.length / key.length))
+        .split("");
+      let answer = [];
+      let j = 0;
+      arrMessage.map((e) => {
+        if (e.codePointAt(0) >= 65 && e.codePointAt(0) <= 90) {
+          let elem = e.codePointAt(0) - 65 - (arrkey[j].codePointAt(0) - 97);
+          j++;
+          if (elem <= 0) elem += 26;
+          answer.push(String.fromCharCode(elem + 65));
+        } else answer.push(e);
+      });
+      return answer.join("");
+    }
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
