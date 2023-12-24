141538abe5d1ab84abeca361e90fd94fd2822baf
tatiana <name>
2023-12-24 22:16:01 +0700

    test

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,54 +21,61 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 class VigenereCipheringMachine {
   constructor(cod) {
-    this.cod = "";
+    this.cod = cod;
   }
 
   encrypt(message, key) {
-    if (message == undefined || key == undefined) {
+    if (!message || !key) {
       throw new Error("Incorrect arguments!");
     }
-    if (this.cod == true || this.cod == "") {
-      let arrMessage = message.toUpperCase().split("");
-      let arrkey = key
+    let arrkey;
+
+    let arrMessage = message.toUpperCase().split("");
+    if (message.length > key.length) {
+      arrkey = key
         .toUpperCase()
         .repeat(Math.ceil(message.length / key.length))
         .split("");
-      let answer = [];
-      let i = 0;
-      arrMessage.map((e) => {
-        if (e.codePointAt(0) >= 65 && e.codePointAt(0) <= 90) {
-          let elem =
-            (e.codePointAt(0) - 65 + (arrkey[i].codePointAt(0) - 65)) % 26;
-          i++;
-          answer.push(String.fromCharCode(elem + 65));
-        } else answer.push(e);
-      });
-      return answer.join("");
-    }
+    } else arrkey = key.toUpperCase().split("");
+    let answer = [];
+    let i = 0;
+    arrMessage.map((e) => {
+      if (e.codePointAt(0) >= 65 && e.codePointAt(0) <= 90) {
+        let elem =
+          (e.codePointAt(0) - 65 + (arrkey[i].codePointAt(0) - 65)) % 26;
+        i++;
+        answer.push(String.fromCharCode(elem + 65));
+      } else answer.push(e);
+    });
+    if (this.cod == false) {
+      return answer.reverse().join("");
+    } else return answer.join("");
   }
   decrypt(encryptedMessage, key) {
-    if (encryptedMessage == undefined || key == undefined) {
+    if (!encryptedMessage || !key) {
       throw new Error("Incorrect arguments!");
     }
-    if (this.cod == false) {
-      let arrMessage = encryptedMessage.toUpperCase().split("");
-      let arrkey = key
+    let arrkey;
+    let arrMessage = encryptedMessage.toUpperCase().split("");
+    if (encryptedMessage.length > key.length) {
+      arrkey = key
         .toUpperCase()
         .repeat(Math.ceil(encryptedMessage.length / key.length))
         .split("");
-      let answer = [];
-      let j = 0;
-      arrMessage.map((e) => {
-        if (e.codePointAt(0) >= 65 && e.codePointAt(0) <= 90) {
-          let elem =
-            (e.codePointAt(0) - 65 - (arrkey[j].codePointAt(0) - 65) + 26) % 26;
-          j++;
-          answer.push(String.fromCharCode(elem + 65));
-        } else answer.push(e);
-      });
-      return answer.join("");
-    }
+    } else arrkey = key.toUpperCase().split("");
+    let answer = [];
+    let j = 0;
+    arrMessage.map((e) => {
+      if (e.codePointAt(0) >= 65 && e.codePointAt(0) <= 90) {
+        let elem =
+          (e.codePointAt(0) - 65 - (arrkey[j].codePointAt(0) - 65) + 26) % 26;
+        j++;
+        answer.push(String.fromCharCode(elem + 65));
+      } else answer.push(e);
+    });
+    if (this.cod == false) {
+      return answer.reverse().join("");
+    } else return answer.join("");
   }
 }
 
